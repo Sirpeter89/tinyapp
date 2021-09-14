94d5963c17b5b3caa6f391a6c46a323784348209
Justin <just2rules@gmail.com>
2021-09-14 15:35:11 -0700

    finished left button

--- index.js
+++ index.js
@@ -5,18 +5,24 @@ const display = document.querySelector(".secondary")
 let index = 0;
 let titles = ["app", "blog", "shop", "note", "game", "grave"]
 
-function getIndex(index, down){
+function getIndex(down){
     if (down){
         if (index-1 < 0){
             index = titles.length-1
+        } else {
+            index--;
         }
     } else {
         if (index+1 > titles.length-1){
             index = 0
+        } else {
+            index++;
         }
     }
 }
 
 leftButton.addEventListener("click", ()=>{
-
+    let down = true;
+    getIndex(down);
+    display.innerHTML = titles[index];
 })
