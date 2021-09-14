f629975f104959f5ee3b77a40a46414ea2e8e737
Justin <just2rules@gmail.com>
2021-09-14 15:31:35 -0700

    started working on listeners

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const leftButton = document.getElementById('slide-left')
-const display = document.querySelector(".display")
+
+const display = document.querySelector(".secondary")
+
+let index = 0;
+let titles = ["app", "blog", "shop", "note", "game", "grave"]
+
+function getIndex(index, down){
+    if (down){
+        if (index-1 < 0){
+            index = titles.length-1
+        }
+    } else {
+        if (index+1 > titles.length-1){
+            index = 0
+        }
+    }
+}
 
 leftButton.addEventListener("click", ()=>{
-    console.log("hello")
+
 })
