e65640918825f157b65170a32157b7f23cfa0f6c
Justin <just2rules@gmail.com>
2021-09-14 15:10:47 -0700

    having issues with script

---
+++ index.js
@@ -0,0 +1,7 @@
+const leftButton = document.getElementById('slide-left')
+console.log(leftButton.innerText)
+
+// leftButton.addEventListener("click", ()=>{
+//     console.log("hello")
+//     leftButton.innerHTML = "Yes"
+// })
